43d169d6e2931d83121cdda550851b84c28d6a0b
Stanislau Marazevich <stas.morozevich@gmail.com>
2021-10-22 13:21:39 +0300

    Add port configurations

--- app/src/server.ts
+++ app/src/server.ts
@@ -10,13 +10,19 @@ const PORT = process.env.PORT || 3000;
 
 async function main() {
   const connectionOptions = await getConnectionOptions();
-  await connection.create(connectionOptions);
-  app.set('port', PORT);
 
-  const server = http.createServer(app);
-  server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-  });
+  try {
+    await connection.create(connectionOptions);
+    app.set('port', PORT);
+
+    const server = http.createServer(app);
+    server.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  } catch (e) {
+    console.error("Can't connect to db", e);
+    process.exit(1);
+  }
 }
 
 main();
